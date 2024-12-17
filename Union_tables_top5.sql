77f9f38c0147933aa3a545904098a7fb03373c9f
MElyes92 <melyes.jouini@gmail.Com>
2024-12-17 12:57:46 +0100

    Update Union_tables_top5.sql

---
+++ Union_tables_top5.sql
@@ -0,0 +1,14 @@
+SELECT *, 'A4' AS youtuber
+FROM `youtube-project-444210.Top_5_entertainment.A4`
+UNION ALL
+SELECT *, 'KIMPRO' AS youtuber
+FROM `youtube-project-444210.Top_5_entertainment.KIMPRO`
+UNION ALL
+SELECT *, 'Stokes_Twins' AS youtuber
+FROM `youtube-project-444210.Top_5_entertainment.Stokes_Twins`
+UNION ALL
+SELECT *, 'alan_chikin_chow' AS youtuber
+FROM `youtube-project-444210.Top_5_entertainment.alan_chikin_chow`
+UNION ALL
+SELECT *, 'Mr_Beast' AS youtuber
+FROM `youtube-project-444210.Mr_beast.MrBeast_last_month_earning`;
